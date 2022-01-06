98c92e6a77b8ec3d93c099de630040bc3ad84247
Dominik Adamiak <me@dom3k.pl>
2022-01-06 16:24:34 +0100

    Day 7 solved

--- src/main/python/day7/crabs.py
+++ src/main/python/day7/crabs.py
@@ -8,10 +8,18 @@ print(f"{crab_table}")
 max_position = max(crab_table)
 current_cheapest_position=(-1,1e16)
 
+
+def arith(n):
+    if n < 2:
+        return n
+    return n * (1+n)/2
+
+
 for current_position_number in range(0, max_position+1):
     current_position_fuel = 0
     for crab in crab_table:
-        current_position_fuel += abs(crab-current_position_number)
+        # to get back to solution to first part, just don't wrap abs with arith
+        current_position_fuel += arith(abs(crab-current_position_number))
     if current_position_fuel < current_cheapest_position[1]:
         current_cheapest_position = current_position_number, current_position_fuel
 
